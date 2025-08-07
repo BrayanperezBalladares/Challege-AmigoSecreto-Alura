674fb9678b70d698c3cd9afb818f6f749aa0aad3
Brayan <bpballadares57@gmail.com>
2025-08-06 22:01:59 -0600

    Agregar función addFriend para capturar, validar y guardar nombres de amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friendsList  = [];
+
+//Referencias al DOM
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+
+function addFriend(){
+    const inputAmigo  = document.getElementById('amigo');
+    const nombre = inputAmigo.value.trim();
+
+    //Validación del campo vacío
+    if (nombre === ''){
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+
+    //Agregar el nombre a la lista
+    friendsList.push(nombre);
+    //Limpiar y enfocar el input
+    inputAmigo.value = '';
+   
+    
+}
